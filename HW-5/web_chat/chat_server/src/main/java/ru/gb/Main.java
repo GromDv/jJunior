06999e704c4d028c83c6270dd6604cfbfbea6008
GromDV <gromdv@gmail.com>
2023-12-24 12:11:55 +0300

    Add HW-5

---
+++ HW-5/web_chat/chat_server/src/main/java/ru/gb/Main.java
@@ -0,0 +1,24 @@
+package ru.gb;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.rmi.UnknownHostException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(1400);
+            Server server = new Server(serverSocket);
+            server.runServer();
+        }
+        catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
